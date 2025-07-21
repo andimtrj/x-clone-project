d726bf6d67aa6e7592ddccf823bb08913e02132b
Andi Mataraja <amataraja@gmail.com>
2025-07-21 23:43:01 +0700

    Adjust root url

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
+'use client'
+import { Button } from "@/components/ui/button";
 import getConfig from "@/firebase/config";
 import { collection, getDocs } from "firebase/firestore";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 
 export default async function Home() {
+  const router = useRouter()
   const { db } = getConfig();
   const foodsCol = collection(db, "foods");
   const usersCol = collection(db, "users");
@@ -14,7 +18,8 @@ export default async function Home() {
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-xl font-light">Hello World</h1>
+      <h1 className="text-xl font-light">Nothing here!</h1>
+      <Button onClick={()=>router.push('/signin')}>Go Sign In!</Button>
     </div>
   );
 }
